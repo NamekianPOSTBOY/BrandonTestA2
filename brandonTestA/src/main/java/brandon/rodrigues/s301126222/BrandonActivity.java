e58d4e9a90002e3d5aeb4c37c97ce5ab5091b5dd
Brandon <rodrigues_Brandon@hotmail.com>
2020-10-31 08:44:19 -0400

    Initial commit Midterm

---
+++ brandonTestA/src/main/java/brandon/rodrigues/s301126222/BrandonActivity.java
@@ -0,0 +1,14 @@
+package brandon.rodrigues.s301126222;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrandonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+} // Brandon Rodrigues 301126222
---
+++ brandonTestA/src/main/java/brandon/rodrigues/s301126222/RodriguesActivity.java
@@ -0,0 +1,14 @@
+package brandon.rodrigues.s301126222;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RodriguesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rodrigues);
+    }
+} // Brandon Rodrigues 301126222
